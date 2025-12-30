b257489c85b45344f1659bbf58a3a1d829751202
Dzarooo <155550980+Dzarooo@users.noreply.github.com>
2025-12-30 22:57:43 +0100

    displaying fetched questions in admin

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,6 +1,6 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc, updateDoc } from "firebase/firestore";
+import { getFirestore, doc, getDoc, updateDoc, getDocs, collection } from "firebase/firestore";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
 
 const Admin = () => {
@@ -27,6 +27,8 @@ const Admin = () => {
         team1: teamOneName,
         team2: teamTwoName,
     }
+
+    const [questions, setQuestions] = useState(null)
 
     const updateTeamName = async (team) => {
         const ref = teamRefs[team];
@@ -88,12 +90,35 @@ const Admin = () => {
             }
         };
 
+        const fetchQuestions = async () => {
+            try {
+                const docsSnap = await getDocs(collection(db, "questions"))
+
+                const data = docsSnap.docs.map(doc => ({
+                    id: doc.id,
+                    answersShown: false,
+                    ...doc.data(),
+                }));
+
+                console.log(data);
+                data[0].answers.map((answer) => {
+                    console.log(answer.answer)
+                })
+                setQuestions(data);
+                console.log("fetched questions.");
+            }
+            catch (err) {
+                console.error(err.message);
+            }
+        }
+
         const unsubAuth = onAuthStateChanged(auth, (user) => {
             if (!user) return;
 
             console.log("Auth ready");
 
             fetchAllTeams();
+            fetchQuestions();
         });
 
         return () => unsubAuth();
@@ -101,15 +126,53 @@ const Admin = () => {
     }, [db]);
 
     return (
-        <div className="w-screen flex justify-between p-5">
-            <div className="flex gap-2 flex-nowrap">
-                <input ref={teamOneName} defaultValue="Loading..." placeholder="Nazwa drużyny 1" className="outline-none border-dotted border-b-4 pt-2 border-b-yellow-300"></input>
-                <button onClick={() => { updateTeamName("team1") }} className="bg-yellow-300 text-black px-2 rounded-lg cursor-pointer">Zapisz</button>
+        <div className="w-screen min-h-screen flex flex-col">
+
+            {/* Teams */}
+            <div className="w-screen flex justify-between p-5">
+                <div className="flex gap-2 flex-nowrap h-fit">
+                    <input ref={teamOneName} defaultValue="Loading..." placeholder="Nazwa drużyny 1" className="outline-none border-dotted border-b-4 pt-2 border-b-yellow-300"></input>
+                    <button onClick={() => { updateTeamName("team1") }} className="bg-yellow-300 text-black px-2 rounded-lg cursor-pointer">Zapisz</button>
+                </div>
+
+                <div className="flex gap-2 flex-nowrap h-fit">
+                    <input ref={teamTwoName} defaultValue="Loading..." placeholder="Nazwa drużyny 2" className="outline-none border-dotted border-b-4 pt-2 border-b-yellow-300"></input>
+                    <button onClick={() => { updateTeamName("team2") }} className="bg-yellow-300 text-black px-2 rounded-lg cursor-pointer">Zapisz</button>
+                </div>
             </div>
 
-            <div className="flex gap-2 flex-nowrap">
-                <input ref={teamTwoName} defaultValue="Loading..." placeholder="Nazwa drużyny 2" className="outline-none border-dotted border-b-4 pt-2 border-b-yellow-300"></input>
-                <button onClick={() => { updateTeamName("team2") }} className="bg-yellow-300 text-black px-2 rounded-lg cursor-pointer">Zapisz</button>
+            {/* Questions */}
+            <div className="flex-1 w-screen">
+                {questions === null && <h1 className="text-center">Loading questions...</h1>}
+
+                {questions != null &&
+                    <div className="w-screen flex items-center flex-col gap-4">
+                        <h1 className="text-center">Questions</h1>
+                        <div className="min-w-fit w-[70vw]">
+                            {questions.map((question, index) => {
+                                return (
+                                    <div key={question.id} className="border-solid border-t border-yellow-300 last:border-b pt-2 pb-4 flex flex-col gap-2">
+                                        <div className="flex flex-nowrap justify-between">
+                                            <p>{index + 1}. {question.question}</p>
+                                            <button className="border-solid border border-yellow-300 px-2 cursor-pointer bg-yellow-300 text-black">{question.answersShown ? "Ukryj" : "Pokaż"}</button>
+                                        </div>
+                                        <div className="mx-6 flex flex-col gap-4">
+                                            {question.answers.map((answer) => (
+                                                <div className="flex justify-between border-dotted border-b-2 border-yellow-300 pb-2">
+                                                    <p>{answer.answer}</p>
+                                                    <div className="flex flex-nowrap gap-4">
+                                                        <p>{answer.value}</p>
+                                                        <button className={`border-solid border border-yellow-300 px-2 cursor-pointer ${answer.isShown ? "bg-transparent text-yellow-300" : "bg-yellow-300 text-black"}`}>{answer.isShown ? "Zakryj" : "Odkryj"}</button>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     )
